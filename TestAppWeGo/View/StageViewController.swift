8e04ae7f67c4e361f19dbaaff0a59d01ea9226bc
Eugene <Eugeniolio@yandex.com>
2021-08-09 22:22:16 +0300

    playerVC made

---
+++ TestAppWeGo/View/StageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StageViewController.swift
+//  TestAppWeGo
+//
+//  Created by Евгений Фирман on 09.08.2021.
+//
+
+import UIKit
+
+class StageViewController: UIViewController {
+    
+    override func viewDidLoad(){
+        
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+        
+    }
+}
